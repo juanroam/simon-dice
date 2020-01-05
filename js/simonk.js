2635da267cb1819f25a43fde6a7911bc05b3273f
juanroam <juanc.rodrigueza@hotmail.com>
2020-01-05 06:58:53 -0500

    Según nivel definido ilumina secuencia de colores
    Función para trasnformar número a colores.
    Funciones para iluminar o apagar un color.
    Función "iluminarSecuencia" que dependiendo del nivel ilumina un color,
    cada segundo, según la secuencia generada.

--- js/simonk.js
+++ js/simonk.js
@@ -9,6 +9,7 @@ class Juego {
   constructor() {
     this.inicializar()
     this.generarSecuencia()
+    this.siguienteNivel()
   }
 
   inicializar() {
@@ -20,7 +21,41 @@ class Juego {
   generarSecuencia() {
     this.secuencia = new Array(10).fill(0).map(n => Math.floor((Math.random() * 4)))
   }
-  
+
+  siguienteNivel() {
+    this.iluminarSecuencia()
+  }
+
+  transformarNumeroAcolor(numero) {
+    switch (numero) {
+      case 0:
+        return 'celeste'
+      case 1:
+        return 'violeta'
+      case 2:
+        return 'naranja'
+      case 3:
+        return 'verde'
+    }
+  }
+
+  iluminarSecuencia() {
+    for (let i = 0; i < this.nivel; i++) {
+      const color = this.transformarNumeroAcolor(this.secuencia[i])
+      // delay entre cada color de un segundo
+      setTimeout(() => this.iluminarColor(color), 1000 * i)
+    }
+  }
+
+  iluminarColor(color) {
+    this.colores[color].classList.add('light')
+    setTimeout(() => this.apagarColor(color), 350)
+  }
+
+  apagarColor(color) {
+    this.colores[color].classList.remove('light')
+  }
+
 }
 
 function empezarJuego() {
